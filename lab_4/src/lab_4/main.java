62d4062be2d12c330c118a1cc7a0c6e75da96682
virazlutskiy <43348085+virazlutskiy@users.noreply.github.com>
2020-09-09 22:35:42 +0300

    Add files via upload

---
+++ lab_4/src/lab_4/main.java
@@ -0,0 +1,101 @@
+package lab_4;
+
+/*1. Создать интерфейс Nameable, с методом getName(), возвращающим
+имя объекта, реализующего интерфейс. Проверить работу для
+различных объектов (например, можно создать классы, описывающие
+разные сущности, которые могут иметь имя: планеты, машины,
+животные и т. д.).
+
+P.S. для простоты реализации и наглядности демонстрации был использован код лабораторной №3
+ */
+
+public class main {
+
+	public static void main(String[] args) {
+		Nameable plate = new Plate("Тарелка",100,"Белая",20,3);
+		Nameable cup = new Cup("Чашка",250,"Черная",200);
+		System.out.println(plate.toString());
+		System.out.println(cup.toString());
+	}
+
+}
+
+interface Nameable{
+	String getName();
+}
+
+abstract class Dish implements Nameable{
+	private String name;
+	private int value;// стоимость одного предмета
+	private String color; //цвет посуды
+	
+	public String getName() {return name;}
+	public int getValue() {return value;}
+	public String getColor() {return color;}
+	
+	public void setName(String name) {this.name = name;}
+	public void setValue(int value) {this.value = value;}
+	public void setColor(String color) {this.color = color;}
+	
+}
+
+class Plate extends Dish{
+	private int diametr;
+	private int amount;
+	
+	public Plate(
+			String name,
+			int value, 
+			String color,
+			int diametr,
+			int amount)
+	{
+		setName(name); //т.к. name - private, а не protected, то установить его можно лишь через метод public
+		setValue(value);
+		setColor(color);
+		this.diametr = diametr;
+		this.amount = amount;
+	}
+	
+	public int getDiametr() {return diametr;}
+	public int getAmount() {return amount;}
+	public int getTottalCost() {return getValue()*amount;}
+	
+	public void setAmount(int amount) {this.amount = amount;}
+	
+	public String toString() {
+		return "Название: " + getName() 
+		+ " | Стоимость: RUB" + getValue() 
+		+ " | Колличество: RUB" + amount 
+		+ " | Итого: " + getTottalCost() 
+		+ " | Цвет: " + getColor() 
+		+ " | Диаметр: " + diametr;
+	}
+}
+
+class Cup extends Dish{
+	private int volume;
+	public Cup(
+			String name,
+			int value, 
+			String color,
+			int volume)
+	{
+		setName(name); //т.к. name - private, а не protected, то установить его можно лишь через метод public
+		setValue(value);
+		setColor(color);
+		this.volume = volume;
+	}
+	
+	public int getVolume() {return volume;}
+	
+	public void setVolume(int volume) {this.volume = volume;}
+	
+	public String toString() {
+		return "Название: " + getName() 
+		+ " | Стоимость: RUB" + getValue() 
+		+ " | Итого: RUB" + getValue() 
+		+ " | Цвет: " + getColor() 
+		+ " | Объём: " + volume;
+	}
+}
